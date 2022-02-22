1d51ec5681574130603de2259c81d533646e728b
James York <kroysemaj@gmail.com>
2022-02-22 14:07:28 -0500

    use babel/eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-  extends: ["react-app", "prettier"],
-  plugins: ["prettier", "jest", "cypress"],
-  parser: "babel-eslint",
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier', 'jest', 'cypress'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+  },
   env: {
     browser: true,
-    "cypress/globals": true,
+    'cypress/globals': true,
     es6: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
-    "prettier/prettier": "warn",
+    'prettier/prettier': 'warn',
   },
 };
